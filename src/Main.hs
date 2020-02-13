16a2b02e8eae530f03feb551d6b3f3678c8beb47
Leo UNO <leouno12@gmail.com>
2020-02-13 20:06:21 +0900

    :construction: wip

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import           System.Random                  ( randomRIO )
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    putStrLn "hello world"
+
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+    dict <- readFile "data/dict.txt"
+    return $ lines dict
+
+minWordLen :: Int
+minWordLen = 5
+
+maxWordLen :: Int
+maxWordLen = 9
+
+gameWords :: IO WordList
+gameWords = filter gameLength <$> allWords
+  where
+    gameLength w = let len = length w in minWordLen < len && len < maxWordLen
+
+randomWord :: IO String
+randomWord = randomWord' =<< gameWords
+
+randomWord' :: WordList -> IO String
+randomWord' ws = do
+    randomIndex <- randomRIO (0, length ws)
+    return $ ws !! randomIndex
